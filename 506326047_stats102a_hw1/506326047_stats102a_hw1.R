0446e8d8128afb0b4d960f328acfdd13a9d6365c
s2230095 <s2230095@ed.ac.uk>
2024-01-18 20:14:00 -0800

    styled using add-on.

--- 506326047_stats102a_hw1/506326047_stats102a_hw1.R
+++ 506326047_stats102a_hw1/506326047_stats102a_hw1.R
@@ -1,56 +1,55 @@
-gcd <- function(x, y){
+gcd <- function(x, y) {
   # Outputs the GCD of two integers.
-  #Args:
-  #x: integer
-  #y: integer
-  #Return:
-  #GCD: integer
-  
-  nums = c(abs(x), abs(y)) #stores x, y in case of later transformation.
-  a = max(nums)            #makes a the higher value and b the lower value
-  b = min(nums)
-  
-  if (a %% b == 0){        #if a mod b = 0 then the min of {a,b} is the GCD.
-    return (b)
-  }
-  else{                    #follow the algorithm described in HW1.pdf
+  # Args:
+  # x: integer
+  # y: integer
+  # Return:
+  # GCD: integer
+
+  nums <- c(abs(x), abs(y)) # stores x, y in case of later transformation.
+  a <- max(nums) # makes a the higher value and b the lower value
+  b <- min(nums)
+
+  if (a %% b == 0) { # if a mod b = 0 then the min of {a,b} is the GCD.
+    return(b)
+  } else { # follow the algorithm described in HW1.pdf
     mod_xy <- a %% b
-    while (mod_xy != 0){
-      mod_xy <-  a %% b
+    while (mod_xy != 0) {
+      mod_xy <- a %% b
       a <- b
       b <- mod_xy
     }
-    return (a)
+    return(a)
   }
 }
 
-lcm <- function(x){
-  #Outputs the LCM of the integers.
-  #Args:
-  #x: vector    n x 1 dimensional vector where 1 < n < 101
-  #Return:
-  #LCM: integer  1-dimensional vector
+lcm <- function(x) {
+  # Outputs the LCM of the integers.
+  # Args:
+  # x: vector    n x 1 dimensional vector where 1 < n < 101
+  # Return:
+  # LCM: integer  1-dimensional vector
 
-  #We can use the relationship that LCM(a, b) = (a * b)/GCD(a, b)
-  #run the code block below till we have a vector with 1 integer value.
-  while (length(x) != 1){
+  # We can use the relationship that LCM(a, b) = (a * b)/GCD(a, b)
+  # run the code block below till we have a vector with 1 integer value.
+  while (length(x) != 1) {
     a <- abs(x[1])
     b <- abs(x[2])
     x <- x[2:length(x)]
-    x[1] <- a * b / gcd(a,b)
+    x[1] <- a * b / gcd(a, b)
   }
   x
 }
 
-is_prime <- function(x){
+is_prime <- function(x) {
   logical_list <- as.logical(1:length(x))
-  for (i in 1:length(x)){
-    if (x[i] == 1 | x[i] == 2){
+  for (i in 1:length(x)) {
+    if (x[i] == 1 | x[i] == 2) {
       logical_list[i] <- TRUE
-    } else{
-        for (val in (x[i] %% (2:(x[i] - 1)))){
-          if (val == 0){
-            logical_list[i] <- FALSE
+    } else {
+      for (val in (x[i] %% (2:(x[i] - 1)))) {
+        if (val == 0) {
+          logical_list[i] <- FALSE
         }
       }
     }
@@ -59,21 +58,21 @@ is_prime <- function(x){
 }
 
 
-exp_count <- function(x, prime){
+exp_count <- function(x, prime) {
   exp_count <- 0
-  for (i in 1:x){
-    if (x %% prime^(i) == 0){
+  for (i in 1:x) {
+    if (x %% prime^(i) == 0) {
       exp_count <- exp_count + 1
     }
   }
   exp_count
 }
 
-prime_divisors <- function(x){
+prime_divisors <- function(x) {
   prime_list <- c()
-  for (i in 2:x){
-    if (is_prime(i)){
-      if (x %% i == 0){
+  for (i in 2:x) {
+    if (is_prime(i)) {
+      if (x %% i == 0) {
         prime_list <- append(prime_list, i)
       }
     }
@@ -82,13 +81,13 @@ prime_divisors <- function(x){
 }
 
 
-get_factors <- function(x){
+get_factors <- function(x) {
   primes <- prime_divisors(x)
   exponents <- 1:length(primes)
-  for (i in 1:length(primes)){
-    p = primes[i]
+  for (i in 1:length(primes)) {
+    p <- primes[i]
     exponents[i] <- exp_count(x, p)
   }
-  factors = list("primes" = primes, "exponents" = exponents)
+  factors <- list("primes" = primes, "exponents" = exponents)
   factors
 }
