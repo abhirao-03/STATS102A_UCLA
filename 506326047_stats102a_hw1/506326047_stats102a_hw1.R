04294f67e68c288c7bc42985560c8fa500b1b21e
s2230095 <s2230095@ed.ac.uk>
2024-01-20 08:19:28 -0800

    added in error handling for all functions.

--- 506326047_stats102a_hw1/506326047_stats102a_hw1.R
+++ 506326047_stats102a_hw1/506326047_stats102a_hw1.R
@@ -5,11 +5,15 @@ gcd <- function(x, y) {
   # y: integer
   # Return:
   # GCD: integer
-
+  
   nums <- c(abs(x), abs(y)) # stores x, y in case of later transformation.
   a <- max(nums) # makes a the higher value and b the lower value
   b <- min(nums)
-
+  
+  if (sum(nums %% 1) != 0){
+    stop('arguments a and b must be integers!')
+  }
+  
   if (a %% b == 0) { # if a mod b = 0 then the min of {a,b} is the GCD.
     return(b)
   } else { # follow the algorithm described in HW1.pdf
@@ -32,6 +36,12 @@ lcm <- function(x) {
 
   # We can use the relationship that LCM(a, b) = (a * b)/GCD(a, b)
   # run the code block below till we have a vector with 1 integer value.
+  if (sum(x %% 1) != 0){
+    stop('x must be a vector of integers!')
+  }
+  if (length(x) < 2){
+    stop('x contains only one element, minimum size for x is 2')
+  }
   
   while (length(x) != 1) {
     a <- abs(x[1])
@@ -49,7 +59,13 @@ is_prime <- function(x) {
   # x: vector    n x 1 dimensional vector where 1 <= n
   # Return:
   # logical_list: list of bools for each i in x.
-
+  if (sum(x %% 1) != 0){
+    stop('x must be a vector of integers!')
+  }
+  if (sum(x < 0) != 0){
+    stop('All elements of x must be positive!')
+  }
+  
   logical_list <- as.logical(1:length(x))
   for (i in 1:length(x)) {
     if (x[i] == 1 | x[i] == 2) { # handle 1 and 2 differently than other primes.
@@ -73,6 +89,9 @@ exp_count <- function(x, prime) {
   # prime: integer  the prime number to raise to x.
   # Return:
   # logical_list: list of bools for each i in x.
+  if (x %% 1 != 0){
+    stop('x must be an integer!')
+  }
   
   exp_count <- 0
   while (x %% prime == 0){
@@ -88,7 +107,10 @@ prime_divisors <- function(x) {
   # x: integer
   # Return
   # prime_list: list   the primes that divide x.
-
+  if (x %% 1 != 0){
+    stop('x must be an integer!')
+  }
+  
   prime_list <- c()
   for (i in 2:x) {
     if (is_prime(i)) {
@@ -107,7 +129,6 @@ get_factors <- function(x) {
   # x:integer
   # Return:
   # factors:list  consisting of 2 nested lists called "primes" and "exponents".
-
   primes <- prime_divisors(x)
   exponents <- 1:length(primes)
   for (i in 1:length(primes)) {
