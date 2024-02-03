8c45238baadfa8dfe81ff1b60b08537a03d8d440
s2230095 <s2230095@ed.ac.uk>
2024-02-03 14:02:25 -0800

    completed tidy_impute() function.

--- 506326047_stats102a_hw2/506326047_stats102a_hw2.R
+++ 506326047_stats102a_hw2/506326047_stats102a_hw2.R
@@ -55,3 +55,91 @@ messy_impute <- function(input_tibble, center = "mean", margin, ...) {
   }
   return(input_tibble)
 }
+
+
+
+tidy_impute <- function(input_tibble, center = "mean", margin, ...) {
+  # Error correcting steps are all the same from messy_impute so we just bring them in.
+  # error handling for if the input_tibble is in fact a tibble
+  if (!is_tibble(input_tibble)) {
+    stop("the input data must be in the format of a tibble!")
+  }
+  
+  if (ncol(input_tibble) != 3) {
+    stop("the input_tibble is likely of the wrong size, keep tibble in the format: UID, Assignments, Marks")
+  }
+  
+  if (!all(c("UID", "Assignments", "Marks") == colnames(input_tibble))){
+    stop("the input_tibble does not have the proper column formats. Must be: 'UID', 'Assignments', 'Marks'")
+  }
+  # error handling for center values not equal to expected inputs.
+  if (center != "mean" & center != "median") {
+    stop("center must be 'mean' or 'median'!")
+  }
+
+  # error handling for margin values not equal to 1 or 2
+  if (!(margin %in% c(1, 2))) {
+    stop("margin must be either 1 or 2!")
+  }
+
+  tidy_na <- which(is.na(input_tibble), arr.ind = TRUE)
+
+  if (center == "mean") {
+    if (margin == 1) {
+      for (ind in 1:length(tidy_na[, 1])) {
+        na_ind <- tidy_na[, 1][ind]
+        na_uid <- unlist(input_tibble[na_ind, ][1], use.names = FALSE)
+        na_student <- filter(input_tibble, input_tibble$UID == na_uid)
+
+        for (i in 1:length(na_student$Marks)) {
+          if (i <= 5) {
+            if (is.na(na_student$Marks[i])) {
+              input_tibble[na_ind, 3] <- mean(na_student$Marks[1:5], na.rm = TRUE, ...)
+            }
+          } else {
+            if (is.na(na_student$Marks[i])) {
+              input_tibble[na_ind, 3] <- mean(na_student$Marks[6:12], na.rm = TRUE, ...)
+            }
+          }
+        }
+      }
+    } else {
+      for (i in 1:length(tidy_na[, 1])) {
+        na_ind <- tidy_na[, 1][i]
+        na_assignment_id <- unlist(input_tibble[na_ind, ][2], use.names = FALSE)
+        na_assignment <- filter(input_tibble, input_tibble$Assignments == na_assignment_id)
+        na_assignment
+        input_tibble[na_ind, 3] <- mean(na_assignment$Marks, na.rm = TRUE)
+      }
+    }
+  } else {
+    if (margin == 1) {
+      for (ind in 1:length(tidy_na[, 1])) {
+        na_ind <- tidy_na[, 1][ind]
+        na_uid <- unlist(input_tibble[na_ind, ][1], use.names = FALSE)
+        na_student <- filter(input_tibble, input_tibble$UID == na_uid)
+
+        for (i in 1:length(na_student$Marks)) {
+          if (i <= 5) {
+            if (is.na(na_student$Marks[i])) {
+              input_tibble[na_ind, 3] <- median(na_student$Marks[1:5], na.rm = TRUE, ...)
+            }
+          } else {
+            if (is.na(na_student$Marks[i])) {
+              input_tibble[na_ind, 3] <- median(na_student$Marks[6:12], na.rm = TRUE, ...)
+            }
+          }
+        }
+      }
+    } else {
+      for (i in 1:length(tidy_na[, 1])) {
+        na_ind <- tidy_na[, 1][i]
+        na_assignment_id <- unlist(input_tibble[na_ind, ][2], use.names = FALSE)
+        na_assignment <- filter(input_tibble, input_tibble$Assignments == na_assignment_id)
+        na_assignment
+        input_tibble[na_ind, 3] <- median(na_assignment$Marks, na.rm = TRUE)
+      }
+    }
+  }
+  return (input_tibble)
+}
